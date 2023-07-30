d46933a47882f33120e9e969eb50d33e4c3a404a
noorteck-sdet <119881887+noorteck-sdet@users.noreply.github.com>
2023-07-30 12:25:30 -0400

    Update DemoDay3.java

--- src/demo/DemoDay3.java
+++ src/demo/DemoDay3.java
@@ -2,12 +2,5 @@ package demo;
 
 public class DemoDay3 {
 
-	public static void main(String[] args) {
-		System.out.println("Demo..");
-		System.out.println("Today");
-		
-	}
-	public static void getInfo(){
-		System.out.println("Adel");
-	}
+	//importance of following stash process && communicating... :)
 }

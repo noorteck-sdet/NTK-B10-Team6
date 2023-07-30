dd07198f276af526fd635712572ad7734ecaa28a
farah <farah@LAPTOP-EQVP0L72.fios-router.home>
2023-07-30 12:29:34 -0400

    Name

--- src/demo/DemoDay3.java
+++ src/demo/DemoDay3.java
@@ -3,4 +3,8 @@ package demo;
 public class DemoDay3 {
 
 	//importance of following stash process && communicating... :)
+	public static void main(String[] args) {
+		System.out.println("Demo..");
+		System.out.println("Today");
+	}
 }

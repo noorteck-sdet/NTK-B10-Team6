498eeabec04a3317c333e8d2fbe07d69a038e4bb
adelalkhalidy <140729981+adelalkhalidy@users.noreply.github.com>
2023-07-30 12:25:02 -0400

    Update DemoDay3.java

--- src/demo/DemoDay3.java
+++ src/demo/DemoDay3.java
@@ -5,5 +5,9 @@ public class DemoDay3 {
 	public static void main(String[] args) {
 		System.out.println("Demo..");
 		System.out.println("Today");
+		
+	}
+	public static void getInfo(){
+		System.out.println("Adel");
 	}
 }
